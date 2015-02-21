e328ee007a74188f662787d606def13cbbb41760
Joseph Wilk <joe@josephwilk.net>
2015-02-21 18:03:36 +0100

    bump

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure for Bukkit Minecraft"
   :dependencies [[org.clojure/clojure         "1.5.1"]
                  [org.clojure/tools.logging   "0.2.6"]
-                 [org.clojure/tools.nrepl     "0.2.3"]
+                 [org.clojure/tools.nrepl     "0.2.7"]
               ;; [org.bukkit/bukkit           "1.6.4-R2.0"]
                  [clojure-complete            "0.2.3"]
                  [cheshire                    "5.2.0"]
